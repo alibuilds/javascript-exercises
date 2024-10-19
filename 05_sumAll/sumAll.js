656616883fa99ad456020f0f52c89460fbd7cba7
Ali Smith <aysmith@users.noreply.github.com>
2024-10-19 13:09:32 -0400

    feat(sumAll): implement function to return sum of all numbers between two integers and fix handling of negative, non-integer, and non-number inputs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b) || typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
+
+
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+
+    let sum = 0;
+
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
